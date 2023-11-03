fd655183757d9ef0d7046a59f87ead3817ac7e7b
ebru <ebrualzey3@gmail.com>
2023-11-03 15:57:03 +0100

    first commit

---
+++ src/Day1/deneme_01.java
@@ -0,0 +1,8 @@
+package Day1;
+
+public class deneme_01 {
+    public static void main(String[] args) {
+
+        System.out.println("deneme1");
+    }
+}
---
+++ src/Day2/deneme_02.java
@@ -0,0 +1,7 @@
+package Day2;
+
+public class deneme_02 {
+    public static void main(String[] args) {
+
+    }
+}
